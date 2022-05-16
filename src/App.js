2bd4ea2303681c9c0b3118edd2a4c26128343778
Pravin kumar Ravi <74667577+GenXDev10426@users.noreply.github.com>
2022-05-16 19:57:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,84 +1,93 @@
-import styled from 'styled-components'
-import axios from 'axios'
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import styled from "styled-components";
+import axios from "axios";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import { IconButton } from "@material-ui/core";
-import './App.css';
-import { Header,LogoImage,AppName, SearchBox, SearchIcon, SearchInput  } from './components/header';
-import Results from './components/Results';
-import { useState } from 'react';
+import "./App.css";
+import {
+  Header,
+  LogoImage,
+  AppName,
+  SearchBox,
+  SearchIcon,
+  SearchInput,
+} from "./components/header";
+import Results from "./components/Results";
+import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  // width: 100%
+  width: 100%;
 `;
-
 
 const ResultListContainer = styled.div`
-display : flex;
-flex-direction: row;
-flex-wrap:wrap;
-gap:20px;
-padding: 30px;
-align-items: center;
-justify-content:center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 30px;
+  align-items: center;
+  justify-content: center;
 `;
 
-
 function App() {
+  const [searchText, setSearchText] = useState();
+  //  const [timeOutId,setTimeOutId] = useState();
+  const [videoList, setVideoList] = useState([]);
 
- const [searchText,setSearchText] = useState(); 
- const [timeOutId,setTimeOutId] = useState(); 
- const [videoList,setVideoList] = useState([]); 
+  const fetchData = async () => {
+    const response = await axios.get(
+      `https://asia-south1-socialboat-dev.cloudfunctions.net/assignmentVideos?q=${searchText}&numResults=42`
+    );
+    console.log(response);
 
- const fetchData = async(searchString) => {
-   const response = await axios.get( `https://asia-south1-socialboat-dev.cloudfunctions.net/assignmentVideos?q=${searchText}&numResults=40`);
-   console.log(response);
+    setVideoList(response.data.results);
+  };
 
-   setVideoList(response.data.results);
- };
+  //  const onTextChange =(e) => {
+  //    clearTimeout(timeOutId);
+  //    setSearchText(e.target.value);
+  //    const timeout= setTimeout ( () => fetchData(e.target.value), 200);
 
-
- const onTextChange =(e) => {
-   clearTimeout(timeOutId);
-   setSearchText(e.target.value);
-   const timeout= setTimeout ( () => fetchData(e.target.value), 200);
-    
-   setTimeOutId(timeout)
- };
+  //    setTimeOutId(timeout)
+  //  };
 
   return (
-    <Container> 
+    <Container>
       <Header>
         <AppName>
           <LogoImage src="/images/logo.svg" />
-            FitnessArc
+          FitnessArc
         </AppName>
         <SearchBox>
           <SearchIcon src="/images/searchIcon.svg" />
           <SearchInput
             placeholder="Search here..."
-             value={searchText}
-             onChange={onTextChange}
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                fetchData();
+              }
+            }}
           />
-         
         </SearchBox>
         <IconButton color="inherit" className="accountIcon">
-        <AccountCircleIcon />
-      </IconButton>
+          <AccountCircleIcon />
+        </IconButton>
       </Header>
-       <ResultListContainer>
-        
-        {
-          videoList?.length? videoList.map((c)=> <Results  key={c.heading}
-          heading={c.heading}
-          tags={c.tags}
-          video={c.video}/> ):"No Results"
-        }
-          
-          
-        
-       </ResultListContainer>
+      <ResultListContainer>
+        {videoList?.length
+          ? videoList.map((c) => (
+              <Results
+                key={c.heading}
+                heading={c.heading}
+                tags={c.tags}
+                video={c.video}
+              />
+            ))
+          : "No Results "}
+      </ResultListContainer>
     </Container>
   );
 }
